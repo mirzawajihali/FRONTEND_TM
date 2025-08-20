4569db43fee2ca9f3ab27ed7d0bfa4d0eefdea12
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-20 22:54:33 +0600

    feat : theme added

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ModeToggle } from './ModeToggler';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <ModeToggle/>
         </div>
     );
 };
