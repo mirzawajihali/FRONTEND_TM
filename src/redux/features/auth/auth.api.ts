aecdd85ba7c6a043e6422a015b4efd15cabc17cc
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-24 18:40:03 +0600

    feat : getting user

--- src/redux/features/auth/auth.api.ts
+++ src/redux/features/auth/auth.api.ts
@@ -25,6 +25,13 @@ export const authApi = baseApi.injectEndpoints({
         data: userInfo,
       }),
     }),
+    userInfo: builder.query({
+      query: () => ({
+        url: "/user/me",
+        method: "GET",
+      }),
+      providesTags: ["USER"],
+    })
    
   }),
 });
