acea4e7c049010890c53ce33055a8e9cdea50ee1
mirzawajihali <mirzawajihali00@gmail.com>
2025-08-26 23:44:02 +0600

    feat : add tour type

--- src/redux/features/tour/tour.api.ts
+++ src/redux/features/tour/tour.api.ts
@@ -4,23 +4,21 @@ export const tourApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     addTourType: builder.mutation({
       query: (tourTypeName) => ({
-        url: "/tour/tour-types",
+        url: "/tour/create-tour-type",
         method: "POST",
         data: tourTypeName,
       }),
+      invalidatesTags: ["TOUR"],
     }),
-   
     getTourTypes: builder.query({
       query: () => ({
         url: "/tour/tour-types",
         method: "GET",
       }),
-      transformResponse : (response) => response.data
-     
+      providesTags: ["TOUR"],
+      transformResponse: (response) => response.data,
     }),
   }),
 });
 
-export const {
-useGetTourTypesQuery
-} = tourApi;
+export const { useGetTourTypesQuery, useAddTourTypeMutation } = tourApi;
